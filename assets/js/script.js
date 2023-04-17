7945d0e419a1e98c88f5858b91f4445c9c25e787
jelly-code <santamaria.javiera@gmail.com>
2023-04-17 09:34:16 -0400

    se agrega validacion de datos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 nombre, carrera, 3 ramos con sus respectivas notas y 
 la nota de aprobacion requerida.
 - El ultimo de los ramos solo permitira ingresar 2 notas, estando la tercera pendiente.
-- Los promedios seran calculados en base a las notas ingresadas y se mostraran en la
+✔️- Los promedios seran calculados en base a las notas ingresadas y se mostraran en la
 quinta columna.
 - El mensaje final (“para aprobar el ramo [ramo]...”), tambien sera calculado por el sistema
 en base a las dos notas ingresadas de ese ramo y la nota de aprobacion indicada.
@@ -42,16 +42,28 @@ function pedirNombreyCarrera() {
 
 
 function pedirDatos() {
-    let ramo = prompt("Ingresa tu ramo:");
-    let dato1 = prompt('Ingresa tu primera nota:');
-    let dato2 = prompt('Ingresa tu segunda nota:');
-    let dato3 = prompt('Ingresa tu tercera nota:');
+    let ramo1 = prompt("Ingresa tu primer ramo:")
+    let ramo2 = prompt("Ingresa tu segundo ramo:")
+    let ramo3 = prompt("Ingresa tu tercer ramo:");
+    let nota1, nota2, nota3;
+  
+  //AQUI RECIBE EL DATO Y VALIDA SI ES NUMERO, SI NO ES NUMERO LO SIGUE SOLICITANDO
+    do {
+    dato1 = prompt('Ingresa tu primera nota:');
+  } while (isNaN(parseFloat(dato1)));
+  do {
+    dato2 = prompt('Ingresa tu segunda nota:');
+  } while (isNaN(parseFloat(dato2)));
+  do {
+    dato3 = prompt('Ingresa tu tercera nota:');
+  } while (isNaN(parseFloat(dato3)));
     
-    let nota1 = parseFloat(dato1)
-    let nota2 = parseFloat(dato2)
-    let nota3 = parseFloat(dato3)
-
-    agregarDatos(ramo, nota1, nota2, nota3);
+  //CON ESTO NOS ENTREGA EL VALOR CON DECIMALES
+  nota1 = parseFloat(dato1);
+  nota2 = parseFloat(dato2);
+  nota3 = parseFloat(dato3);
+  
+  agregarDatos(ramo, nota1, nota2, nota3);
 }
 
 function agregarDatos(ramo, nota1, nota2, nota3) {
@@ -62,14 +74,15 @@ function agregarDatos(ramo, nota1, nota2, nota3) {
     let cell3 = row.insertCell(2);
     let cell4 = row.insertCell(3);
     let cell5 = row.insertCell(4);
+  
+  //AQUI SE CALCULA EL PROMEDIO DE NOTAS
+    let promedioNotas = ((nota1 + nota2 + nota3) / 3).toFixed(1);
 
-    let promedio = (nota1 + nota2 + nota3) / 3;
-
+  //CON .TOFIXED DA EL RESULTADO CON SOLO 1 DECIMAL
     cell1.innerHTML = ramo
-    cell2.innerHTML = nota1
-    cell3.innerHTML = nota2
-    cell4.innerHTML = nota3
+    cell2.innerHTML = nota1.toFixed(1);
+    cell3.innerHTML = nota2.toFixed(1);
+    cell4.innerHTML = nota3.toFixed(1);
 
-    cell5.innerHTML = promedio
+    cell5.innerHTML = promedioNotas
 }
-
